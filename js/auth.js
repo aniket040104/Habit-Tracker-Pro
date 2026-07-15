3f2557d96968c18f7eb5b0b84a730a3ccf9da371
ANIKET SHARMA <291688584+aniket040104@users.noreply.github.com>
2026-07-15 12:31:50 +0530

    Save loggen in user

--- js/auth.js
+++ js/auth.js
@@ -1,28 +1,26 @@
-import { auth } from "./firebase.js";
-
-console.log("auth.js loaded");
-import {
-  GoogleAuthProvider,
-  signInWithPopup
-} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
-
-const provider = new GoogleAuthProvider();
-
 window.googleLogin = async function () {
   try {
     const result = await signInWithPopup(auth, provider);
 
     const user = result.user;
 
-    console.log("✅ Login Successful");
+    localStorage.setItem(
+      "habitTrackerUser",
+      JSON.stringify({
+        uid: user.uid,
+        name: user.displayName,
+        email: user.email,
+        photo: user.photoURL
+      })
+    );
 
+    console.log("✅ Login Successful");
     console.log(user);
 
+    location.reload();
+
   } catch (error) {
-
     console.error(error);
-
     alert(error.message);
-
   }
 };
